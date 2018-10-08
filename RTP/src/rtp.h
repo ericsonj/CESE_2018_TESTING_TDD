7c3091034c080aabe23746a134005e6751452391
Ericson Joseph <ericsonjoseph@gmail.com>
2018-10-07 19:28:52 -0500

    Primer test que falla para probar el funcionamiento del framework

---
+++ RTP/src/rtp.h
@@ -0,0 +1,21 @@
+/**
+ * File:   rtp.h
+ * Author: Ericson Joseph
+ *
+ * Created on October  7, 2018,  7:18 PM
+ */
+
+#ifndef __RTP_H
+#define __RTP_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void RTP_Init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __RTP_H */
---
+++ RTP/test/test_rtp.c
@@ -0,0 +1,19 @@
+/**
+ * File:   test_rtp.c
+ * Author: Ericson Joseph
+ *
+ * Created on October  7, 2018,  7:12 PM
+ */
+
+#include "unity.h"
+#include "rtp.h"
+
+void setUp(void) {
+}
+
+void tearDown(void) {
+}
+
+void test_RTP_Init(void) {
+    TEST_FAIL();
+}
